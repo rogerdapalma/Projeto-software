bdc07e616727aa89fb589f0e15cc5f368ed2a773
Roger <94903084+rogerdapalma@users.noreply.github.com>
2023-12-11 18:55:37 -0300

    Delete for MEANI/CRUD directory

--- for MEANI/CRUD/src/main/java/com/example/CRUD/Repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CRUD.Repository;
-
-import com.example.CRUD.Model.Pessoa;
-import org.springframework.data.repository.CrudRepository;
-
-// CRUD - Create, Read, Update, Delete da classe Pessoa
-public interface PessoaRepository extends CrudRepository <Pessoa, Integer> {
-
-}
